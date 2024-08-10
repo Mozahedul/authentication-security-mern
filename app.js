8d4a75b25a8b29067c3294348aa6fc70248d25d4
Mozahedul <mozahed001@gmail.com>
2024-08-10 22:07:40 +0600

    Add level 1 authentication on login and register

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const mongoose = require("mongoose");
 
 const app = express();
 dotenv.config();
@@ -7,6 +8,29 @@ dotenv.config();
 app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Connect to MongoDB with mongoose ODM
+const connectToDB = async () => {
+  const uri = process.env.MONGO_URI;
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB Successfully");
+  } catch (error) {
+    console.log("DATABASE CONNECTION ERROR: => ", error);
+  }
+};
+
+connectToDB();
+
+// Create register schema
+const userSchema = mongoose.Schema({
+  name: String,
+  password: String,
+});
+
+// Create User model
+const User = mongoose.model("User", userSchema);
 
 app.get("/", function (req, res) {
   res.render("home");
@@ -17,6 +41,44 @@ app.get("/login", function (req, res) {
 });
 app.get("/register", function (req, res) {
   res.render("register");
+});
+
+app.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const newUser = new User({
+      name: username,
+      password,
+    });
+
+    const savedUser = await newUser.save();
+    if (savedUser) {
+      res.render("secrets");
+    } else {
+      res.send("User does not created");
+    }
+  } catch (error) {
+    console.log("Error on Registration: => ", error);
+  }
+});
+
+app.post("/login", async function (req, res) {
+  const { username, password } = req.body;
+  try {
+    const foundUser = await User.findOne({ name: username });
+
+    if (foundUser) {
+      if (foundUser.password === password) {
+        res.render("secrets");
+      } else {
+        console.log("The password you entered is not correct");
+      }
+    } else {
+      console.log("USER NOT FOUND", username);
+    }
+  } catch (error) {
+    console.log("Error in login", error);
+  }
 });
 
 const port = process.env.PORT || 3001;
